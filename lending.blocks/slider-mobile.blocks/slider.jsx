f5896178063da3abbb63a2a99f38287a06c44446
MichailDenisov <104084982+MichailDenisov@users.noreply.github.com>
2023-04-28 23:22:50 +0500

    arrow__play

--- lending.blocks/slider-mobile.blocks/slider.jsx
+++ lending.blocks/slider-mobile.blocks/slider.jsx
@@ -11,46 +11,49 @@ const img = [
 
 ];
 
+const sliderWrapper = document.querySelector(".picture-slider");
+const sliderImages = sliderWrapper.querySelector(".pic__sl");
+const sliderArrows = sliderWrapper.querySelectorAll(".slider__arrow");
+
+let interiorNumber = 0;
+
 initSlider(img)
 
 function initSlider(images, options) {
-if (!images || !images.length) return;
+if (!images || !images.length) return;    
 
     options = options || {
         dots: false,
         titles: false,
         autopley: false,
         autoplayInterval: 3000
-    }
+    }    
 
-    const sliderWrapper = document.querySelector(".picture-slider");
-    const sliderImages = sliderWrapper.querySelector(".pic__sl");
-    const sliderArrows = sliderWrapper.querySelector(".slider-arrows");
-
-    initImages();
-    initArrows();
+    initImages(images);
+    initArrows(images);
 
     if (options.dots) {
-        // initDots();
+        initDots();
     }
 
     if (options.autoplay) {
-        // initAutopley();
+        initAutopley();
     }
 
-    function initImages() {
+}
+
+    function initImages(images) {
         images.forEach((image, index) => {
         document.querySelector('.images-list').innerHTML +=
         `<img class="pic__sl picture-slider__adm image n${index}" src="${image.url}"alt="Rostov-on-Don">`
         });
     }
-}
 
-function initArrows() {
+function initArrows(images) {
     let lastindex = images.length - 1;
-    sliderArrows.querySelector(".slider-arrows").forEach(arrow => {
-        arrow.querySelector("click", function() {
-            let interiorNumber = +sliderImages.querySelector(".active").detaset.index;
+    sliderArrows.forEach(arrow => {
+        arrow.addEventListener("click", function() {
+            // let interiorNumber = +sliderImages.querySelector(".active").detaset.index;
             let nextNumber;
             if (arrow.classList.contains("left")) {
                 nextNumber = interiorNumber === 0? lastindex : interiorNumber - 1;
@@ -58,8 +61,12 @@ function initArrows() {
                 nextNumber = interiorNumber === lastindex? 0 : interiorNumber + 1;
 
             }
+            interiorNumber = nextNumber
             moveSlider(nextNumber);
         });
     });
 }
 
+function moveSlider(nextNumber){
+    console.log(nextNumber)
+}
